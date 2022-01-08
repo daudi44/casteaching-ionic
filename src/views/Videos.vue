1f69dc9bf7d829951a859dc49a7fb801a80780d0
daudi44 <daudi@iesebre.com>
2022-01-08 13:37:53 +0100

    Fi llista de Real World

--- src/views/Videos.vue
+++ src/views/Videos.vue
@@ -1,31 +1,142 @@
 <template>
-  <ion-list>
-    <ion-list-header>
-      Llistat de Videos
-    </ion-list-header>
+  <ion-content>
+    <ion-refresher slot="fixed" @ion-Refresh="refresh" id="refresher">
+      <ion-refresher-content></ion-refresher-content>
+    </ion-refresher>
+    <ion-list>
+      <ion-list-header>
+        Llistat de Videos
+      </ion-list-header>
 
-    <ion-item v-for="video in videos" :key="video.id">
-      <ion-avatar slot="start">
-        <img src="https://via.placeholder.com/80">
-      </ion-avatar>
-      <ion-label>
-        <h2>{{ video.title }}</h2>
-        <p>{{ video.description }}</p>
-      </ion-label>
-    </ion-item>
+      <ion-item v-for="video in videos" :key="video.id">
+        <ion-avatar slot="start">
+          <img src="https://via.placeholder.com/80">
+        </ion-avatar>
+        <ion-label>
+          <h2>{{ video.title }}</h2>
+          <p>{{ video.description }}</p>
+        </ion-label>
+      </ion-item>
 
-  </ion-list>
+    </ion-list>
+    <ion-list id="skeleton" v-if="loading">
+      <ion-list-header>
+        <ion-skeleton-text animated style="width: 80px"></ion-skeleton-text>
+      </ion-list-header>
+      <ion-item>
+        <ion-avatar slot="start">
+          <ion-skeleton-text></ion-skeleton-text>
+        </ion-avatar>
+        <ion-label>
+          <h3>
+            <ion-skeleton-text animated style="width: 80%"></ion-skeleton-text>
+          </h3>
+          <p>
+            <ion-skeleton-text animated style="width: 60%"></ion-skeleton-text>
+          </p>
+        </ion-label>
+      </ion-item>
+      <ion-item>
+        <ion-avatar slot="start">
+          <ion-skeleton-text></ion-skeleton-text>
+        </ion-avatar>
+        <ion-label>
+          <h3>
+            <ion-skeleton-text animated style="width: 80%"></ion-skeleton-text>
+          </h3>
+          <p>
+            <ion-skeleton-text animated style="width: 60%"></ion-skeleton-text>
+          </p>
+        </ion-label>
+      </ion-item>
+      <ion-item>
+        <ion-avatar slot="start">
+          <ion-skeleton-text></ion-skeleton-text>
+        </ion-avatar>
+        <ion-label>
+          <h3>
+            <ion-skeleton-text animated style="width: 80%"></ion-skeleton-text>
+          </h3>
+          <p>
+            <ion-skeleton-text animated style="width: 60%"></ion-skeleton-text>
+          </p>
+        </ion-label>
+      </ion-item>
+      <ion-item>
+        <ion-avatar slot="start">
+          <ion-skeleton-text></ion-skeleton-text>
+        </ion-avatar>
+        <ion-label>
+          <h3>
+            <ion-skeleton-text animated style="width: 80%"></ion-skeleton-text>
+          </h3>
+          <p>
+            <ion-skeleton-text animated style="width: 60%"></ion-skeleton-text>
+          </p>
+        </ion-label>
+      </ion-item>
+      <ion-item>
+        <ion-avatar slot="start">
+          <ion-skeleton-text></ion-skeleton-text>
+        </ion-avatar>
+        <ion-label>
+          <h3>
+            <ion-skeleton-text animated style="width: 80%"></ion-skeleton-text>
+          </h3>
+          <p>
+            <ion-skeleton-text animated style="width: 60%"></ion-skeleton-text>
+          </p>
+        </ion-label>
+      </ion-item>
+      <ion-item>
+        <ion-avatar slot="start">
+          <ion-skeleton-text></ion-skeleton-text>
+        </ion-avatar>
+        <ion-label>
+          <h3>
+            <ion-skeleton-text animated style="width: 80%"></ion-skeleton-text>
+          </h3>
+          <p>
+            <ion-skeleton-text animated style="width: 60%"></ion-skeleton-text>
+          </p>
+        </ion-label>
+      </ion-item>
+      <ion-item>
+        <ion-avatar slot="start">
+          <ion-skeleton-text></ion-skeleton-text>
+        </ion-avatar>
+        <ion-label>
+          <h3>
+            <ion-skeleton-text animated style="width: 80%"></ion-skeleton-text>
+          </h3>
+          <p>
+            <ion-skeleton-text animated style="width: 60%"></ion-skeleton-text>
+          </p>
+        </ion-label>
+      </ion-item>
+    </ion-list>
+  </ion-content>
 </template>
 
 <script>
 import casteaching from "@acacha/casteaching";
-import {IonAvatar, IonItem, IonLabel, IonList, IonListHeader} from "@ionic/vue";
+import {
+  IonAvatar,
+  IonContent,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonListHeader,
+  IonRefresher,
+  IonRefresherContent, IonSkeletonText
+} from "@ionic/vue";
 
 export default {
   name: "Videos",
   data(){
     return{
-      videos:[]
+      videos:[],
+      loading: true
     }
   },
   components: {
@@ -33,10 +144,27 @@ export default {
     IonItem,
     IonListHeader,
     IonAvatar,
-    IonLabel
+    IonLabel,
+    IonRefresher,
+    IonRefresherContent,
+    IonContent,
+    IonSkeletonText
+  },
+  methods:{
+    async refresh(){
+      this.fetchVideos()
+      this.refresher.complete()
+    },
+    async fetchVideos(){
+      this.videos = await casteaching.videos()
+    }
   },
   async created() {
-    this.videos = await casteaching.videos()
+    this.fetchVideos()
+    this.loading = false
+  },
+  mounted() {
+    this.refresher = document.getElementById('refresher')
   }
 }
 </script>
