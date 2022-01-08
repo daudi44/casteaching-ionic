a6cc868e74520723c19edd2b9735e3a782d4ba91
daudi44 <daudi@iesebre.com>
2022-01-08 12:23:35 +0100

    Fi del video 104

---
+++ src/views/Videos.vue
@@ -0,0 +1,46 @@
+<template>
+  <ion-list>
+    <ion-list-header>
+      Llistat de Videos
+    </ion-list-header>
+
+    <ion-item v-for="video in videos" :key="video.id">
+      <ion-avatar slot="start">
+        <img src="https://via.placeholder.com/80">
+      </ion-avatar>
+      <ion-label>
+        <h2>{{ video.title }}</h2>
+        <p>{{ video.description }}</p>
+      </ion-label>
+    </ion-item>
+
+  </ion-list>
+</template>
+
+<script>
+import casteaching from "@acacha/casteaching";
+import {IonAvatar, IonItem, IonLabel, IonList, IonListHeader} from "@ionic/vue";
+
+export default {
+  name: "Videos",
+  data(){
+    return{
+      videos:[]
+    }
+  },
+  components: {
+    IonList,
+    IonItem,
+    IonListHeader,
+    IonAvatar,
+    IonLabel
+  },
+  async created() {
+    this.videos = await casteaching.videos()
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
